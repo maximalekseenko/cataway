80d7865c9a60c15ffe308f37cb34879b04237359
maximalekseenko <max.alekseenko@gmail.com>
2022-12-30 14:52:44 +0300

    release now works

--- build-release.sh
+++ build-release.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
 # create a key
-rmdir -p ./keystores/
-mkdir -p ./keystores/
-keytool -genkey -v -keystore ./keystores/key01.keystore -alias key01 -keyalg RSA -keysize 2048 -validity 10000
-
-# export variables
-export P4A_RELEASE_KEYSTORE=./keystores/key01.keystore
-export P4A_RELEASE_KEYSTORE_PASSWD=olesya
-export P4A_RELEASE_KEYALIAS_PASSWD=olesya
-export P4A_RELEASE_KEYALIAS=key01
+keytool -genkey -v -keystore ./keystores/cataway.keystore -alias ca-key -keyalg RSA -keysize 2048 -validity 10000
 
 # build
 buildozer -v android release
 
+# sign
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./keystores/cataway.keystore ./bin/*release.aab ca-key
+
 # optimize
-.buildozer/android/platform/android-sdk-20/build-tools/23.0.1/zipalign -v 4 ./bin/Your-App-0.1-release.apk ./bin/Your-App-0.1-release-optimized.apk
+# $ .buildozer/android/platform/android-sdk-21/tools/zipalign -v 4 ./saythis/bin/SayThis-1.1.6-release-unsigned.apk ./saythis/bin/SayThis.apk
